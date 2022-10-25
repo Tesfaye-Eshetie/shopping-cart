9823830c7ac0ad65971e92dabfe343fba7317c8c
Tesfaye Eshetie <73901218+Tesfaye-Eshetie@users.noreply.github.com>
2022-10-25 09:44:46 -0400

    Format currency by adding  utilities

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
+import { formatCurrency } from "../utilities/formatCurrency";
 
 type StoreItemProps = {
   id: number;
@@ -16,6 +17,12 @@ export default function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         height="200px"
         style={{ objectFit: "cover" }}
       />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+        </Card.Title>
+      </Card.Body>
     </Card>
   );
 }
